d8d83733b6c333b987176351a1b563d4fc7d248a
Drew Nutter <nutter@nodalexchange.com>
2017-08-11 19:52:55 -0400

    Now more flexible and generic: - Decorator defaults to using base class method on empty instance when locked, or you can set custom flag for your own solution. - Metaclass now looks for 'lock' attribute in class definition to determine whether which classes methods it should lock (defaults to all base class methods but no local methods). - Decorator outside of class for easier use. - Split operations out of __new__ into more generic methods.

--- locker.py
+++ locker.py
@@ -1,18 +1,95 @@
+def locker(custom=False):
+	"""Decorator factory that decorates a method of a class that has inherited
+	one other class. It changes the method's behavior based on the "custom"
+	parameter, the state of the instance attribute "_locked", and the presense
+	of an identically named method in the base class.
+	
+	* When there is no method in the base class with the name of the decorated
+	function, this decorator has no effect.
+
+	_locked:
+		True: Depends on "custom" parameter:
+			True: Execute body of decorated function.
+			False: Execute base class's version of this method against an
+				   empty instance of the base class, return the result. There
+				   are no side effects.
+		False: Execute base class's version of this method against this
+			   instance, with any associated side effects to this instance.
+	"""
+	def decorator(f):
+		def new_f(self, *args, **kwargs):
+			try:
+				base = self.__class__.__bases__[0]
+				base_f = getattr(base, f.__name__)
+				if self._locked:
+					return (f if custom else base_f)(base(), *args, **kwargs)
+			except AttributeError:
+				return f(self, *args, **kwargs)
+			return base_f(self, *args, **kwargs)
+		return new_f
+	return decorator
+
+
 class Locker(type):
 	""" Use this as a metaclass to create a container class that behaves as if
 	it is empty until it is unlocked. This can only be used on classes with a
 	__getitem__ method. Use this on a subclass of the container to lock.
 	"""
-	def __new__(cls, name, bases, local):
+	ignore = {'__getattribute__', '__new__', '__call__', '__subclasshook__',
+			  '__init__', 'lock', 'unlock', '__init_subclass__'}
+
+	def __new__(mcs, name, bases, local):
+		base_class_names = set()
+		classes_methods = []
+		for base_class in bases:
+			base_class_names.add(base_class.__name__)
+			# Base methods: low priority
+			classes_methods.append((base_class.__name__,
+									mcs._get_methods(base_class)))
+		classes_methods.append(('local', local))    # Locals: medium priority
+		try:
+			to_lock = local['lock']
+			if 'bases' in to_lock:
+				to_lock.remove('bases')
+				to_lock |= base_class_names
+		except KeyError:
+			to_lock = base_class_names
 		new_local = {}
+		for class_name, methods in classes_methods:
+			if class_name in to_lock:
+				new_local.update(mcs._lock_methods(methods))
+			else:
+				new_local.update(methods)
+		new_local.update(mcs._locking_methods())    # Locking: high priority
+		return type.__new__(mcs, name, bases, new_local)
+
+	@staticmethod
+	def _get_methods(clss):
+		return {mth_name: getattr(clss, mth_name) for mth_name in dir(clss)
+				if callable(getattr(clss, mth_name)) and mth_name != '__class__'}
+	
+	@classmethod
+	def _lock_methods(mcs, methods):
+		locked_methods = {}
+		for method_name, method in methods.items():
+			if method_name not in mcs.ignore and callable(method):
+				locked_methods[method_name] = locker()(method)
+			else:
+				locked_methods[method_name] = method
+		return locked_methods
+	
+	@staticmethod
+	def _locking_methods():
+		methods = {}
 		
 		def __init__(self, req, *args, **kwargs):
 			self.__class__.__bases__[0].__init__(self, *args, **kwargs)
 			try:
 				self.__class__.__bases__[0].__getitem__(self, req)
 			except LookupError as e:
-				raise e.__class__('Provided index must exist. This instance'
-								 ' cannot be unlocked.')
+				key_index = e.__class__.__name__.rstrip('Error').lower()
+				raise e.__class__('Provided {} must exist. '.format(key_index)
+								  + 'This instance cannot be unlocked.')
 			if hasattr(req, '__iter__'):
 				self._req = set(req)
 			else:
@@ -29,28 +106,7 @@ class Locker(type):
 				return True
 			return False
 		
-		for base in bases:
-			for method_name, method in inspect.getmembers(base):
-				if callable(method) and method_name not in \
-						('__getattribute__', '__new__', '__call__',
-						 '__subclasshook__', '__class__', '__init__', 'lock',
-						 'unlock', '__init_subclass__'):
-					new_local[method_name] = cls.locker(method)
-		new_local.update(local)
-		new_local['__init__'] = __init__
-		new_local['lock'] = lock
-		new_local['unlock'] = unlock
-		return type.__new__(cls, name, bases, new_local)
-	
-	@staticmethod
-	def locker(f):
-		def new_f(self, *args, **kwargs):
-			try:
-				base = self.__class__.__bases__[0]
-				base_f = getattr(base, f.__name__)
-				if self._locked:
-					return base_f(base(), *args, **kwargs)
-			except AttributeError:
-				return f(self, *args, **kwargs)
-			return base_f(self, *args, **kwargs)
-		return new_f
+		methods['__init__'] = __init__
+		methods['lock'] = lock
+		methods['unlock'] = unlock
+		return methods
